487a9ea5e1437c9373c28d6d84d0c8c407d15fcb
MoNi-hub4 <moniitvf@gmail.com>
2023-03-09 01:50:09 +0530

    mbfinal

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const iconMenuOpen = document.querySelector(".icon-menu-open");
 const navListContainer = document.querySelector(".nav-list-container");
 const menuClose = document.querySelector(".menu-close");
+const body = document.querySelector("body");
 
 iconMenuOpen.addEventListener('click', (e) => {
     navListContainer.classList.add('open');
     iconMenuOpen.classList.add('invisible');
+    body.classList.add('background-active');
 })
 
 menuClose.addEventListener('click' , (e) => {
     navListContainer.classList.remove('open');
     iconMenuOpen.classList.remove('invisible');
+    body.classList.remove('background-active');
 })
